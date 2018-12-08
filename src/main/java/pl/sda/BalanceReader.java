904200a9d7672f40061060c25bcb548b80cafd9d
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2018-12-08 11:58:06 +0100

    excel

--- src/main/java/pl/sda/BalanceReader.java
+++ src/main/java/pl/sda/BalanceReader.java
@@ -1,8 +1,40 @@
 package pl.sda;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+
 public class BalanceReader {
 
-    BalanceData read() {
+    private static final String FILE_PATH = "C:\\budzet_kowalskich.xls";
+
+    BalanceData read() throws IOException, InvalidFormatException {
+
+        try (InputStream inp = new FileInputStream(FILE_PATH)) {
+            Workbook wb = WorkbookFactory.create(inp);
+            Sheet sheet = wb.getSheetAt(0);
+            for (int i = 1; i < sheet.getLastRowNum(); i++) {
+
+                Row row = sheet.getRow(i);
+                BigDecimal income = getCellValue(row, 1);
+                BigDecimal outcome = getCellValue(row, 3);
+
+                System.out.println("Dochód: " + income);
+                System.out.println("Wydatek: " + outcome);
+            }
+        }
         return null;
     }
+
+
+    private BigDecimal getCellValue(Row row, int columnNumber) {
+        Cell cell = row.getCell(columnNumber);
+        return !cell.toString().isEmpty() ?
+                new BigDecimal(cell.toString()) : BigDecimal.ZERO;
+    }
+
 }
--- src/test/java/pl/sda/BalanceReaderTest.java
+++ src/test/java/pl/sda/BalanceReaderTest.java
@@ -1,6 +1,9 @@
 package pl.sda;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +11,21 @@ class BalanceReaderTest {
 
     @Test
     void read() {
+        BalanceReader reader = new BalanceReader();
+        BalanceData data = reader.read();
+
+        Assertions.assertEquals(
+                data.getIncomes().size(), 3);
+
+        Assertions.assertTrue(
+                data.getIncomes()
+                        .contains(new BigDecimal("20000")));
+
+        Assertions.assertEquals(
+                data.getOutcomes().size(), 16);
+
+        Assertions.assertTrue(
+                data.getOutcomes()
+                        .contains(new BigDecimal("126")));
     }
 }
