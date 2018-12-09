b0402dc6edd7bcbbb6d01416ed8d03f11e720175
n2god <n2god@o2.pl>
2018-12-09 14:39:48 +0100

    calculate with regex

--- src/main/java/pl/sda/StringSum.java
+++ src/main/java/pl/sda/StringSum.java
@@ -13,8 +13,11 @@ public class StringSum {
         String input = sc.next();
 
         int suma = 0;
-        String s = (input.replaceAll("\\D", " "));
+        String s = input.replaceAll("--", "- -");
+        s = s.replaceAll("[^0-9\\-]", " ");
+        s = s.replaceAll("-", " - ");
         s = s.replaceAll("  ", " ");
+
         String[] s1 = s.split(" ");
         for (String number : s1) {
             if (!number.isEmpty()){
