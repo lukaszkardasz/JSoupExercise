e462ad7116ab64d1df4856ce3e41bfbcc753a05d
n2god <n2god@o2.pl>
2018-12-09 14:10:17 +0100

    calculate with regex

--- src/main/java/pl/sda/StringSum.java
+++ src/main/java/pl/sda/StringSum.java
@@ -1,6 +1,29 @@
 package pl.sda;
 
+import java.util.Scanner;
+
 public class StringSum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Wprowadź ciąg znaków:");
+
+        String input = sc.next();
+
+        int suma = 0;
+        String s = (input.replaceAll("\\D", " "));
+        s = s.replaceAll("  ", " ");
+        String[] s1 = s.split(" ");
+        for (String number : s1) {
+            if (!number.isEmpty()){
+                suma += Integer.parseInt(number);
+            }
+        }
+        System.out.println(suma);
+    }
+
 
 
 }
